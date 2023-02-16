b7e186343986f91174d3d3b8b3e004e6db5f0d85
asTravars33 <jess.wang37@gmail.com>
2023-02-16 13:36:22 -0500

    Fix bug when quest menu is accessed midway through a quest

--- app/src/main/java/com/wangjessica/taskmaster/QuestMenuActivity.java
+++ app/src/main/java/com/wangjessica/taskmaster/QuestMenuActivity.java
@@ -3,15 +3,20 @@ package com.wangjessica.taskmaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class QuestMenuActivity extends AppCompatActivity {
 
-    ArrayList<String> tasks;
-    ArrayList<String> times;
+    private ArrayList<String> tasks;
+    private ArrayList<String> times;
+    private ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -22,9 +27,15 @@ public class QuestMenuActivity extends AppCompatActivity {
         Intent intent = getIntent();
         tasks = intent.getStringArrayListExtra("tasks");
         times = intent.getStringArrayListExtra("times");
+        layout = findViewById(R.id.layout);
     }
     // Moving to the quest types
     public void questPreset(View view){
+        if(tasks==null || times==null){
+            Snackbar snackbar = Snackbar.make(layout, "Please start quest from an open to-do list!", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return;
+        }
         Intent intent = new Intent(QuestMenuActivity.this, SingleQuestActivity.class);
         intent.putStringArrayListExtra("tasks", tasks);
         intent.putStringArrayListExtra("times", times);
@@ -32,6 +43,11 @@ public class QuestMenuActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void questTemplate(View view){
+        if(tasks==null || times==null){
+            Snackbar snackbar = Snackbar.make(layout, "Please start quest from an open to-do list!", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return;
+        }
         Intent intent = new Intent(QuestMenuActivity.this, SingleQuestActivity.class);
         intent.putStringArrayListExtra("tasks", tasks);
         intent.putStringArrayListExtra("times", times);
@@ -39,6 +55,11 @@ public class QuestMenuActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void questAI(View view){
+        if(tasks==null || times==null){
+            Snackbar snackbar = Snackbar.make(layout, "Please start quest from an open to-do list!", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return;
+        }
         Intent intent = new Intent(QuestMenuActivity.this, SingleQuestActivity.class);
         intent.putStringArrayListExtra("tasks", tasks);
         intent.putStringArrayListExtra("times", times);
